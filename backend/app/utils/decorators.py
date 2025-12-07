686cac84b800de4f3a8dee76034fcf063a00318a
Aksh2758 <akshu290110@gmail.com>
2025-12-07 10:47:20 +0530

    fix: resolve circular import in decorators using current_app

--- backend/app/utils/decorators.py
+++ backend/app/utils/decorators.py
@@ -2,7 +2,11 @@ import jwt
 from functools import wraps
 from flask import request, jsonify, current_app
 from bson import ObjectId
-from .. import mongo
+
+def get_mongo():
+    """Helper function to get mongo instance from current_app"""
+    from flask_pymongo import PyMongo
+    return current_app.extensions['pymongo']['MONGO'][0]
 
 def token_required(f):
     @wraps(f)
@@ -13,6 +17,7 @@ def token_required(f):
             return jsonify({"error": "Token missing"}), 401
 
         try:
+            mongo = get_mongo()
             token = token.replace("Bearer ", "")
             data = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=["HS256"])
             user = mongo.db.users.find_one({"_id": ObjectId(data['user_id'])})
